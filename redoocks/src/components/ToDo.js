faa6967af59be8fe3bd1f1868d34a61dce303e56
Yumina9 <saei5455@naver.com>
2020-05-19 17:30:27 +0900

    Refactoring with Context part Two

---
+++ redoocks/src/components/ToDo.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { COMPLETE, DEL, UNCOMPLETE } from "../actions";
+import { useDispatch } from "../context";
+export default ({ text, id, isCompleted }) => {
+  const dispatch = useDispatch();
+  return (
+    <li>
+      <span>{text}</span>
+      <span onClick={() => dispatch({ type: DEL, payload: id })}>❌</span>
+      <span
+        onClick={() =>
+          dispatch({ type: isCompleted ? UNCOMPLETE : COMPLETE, payload: id })
+        }
+      >
+        {isCompleted ? " 🙅🏻‍♀️" : "✅"}
+      </span>
+    </li>
+  );
+};
