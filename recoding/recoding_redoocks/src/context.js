6e719f7b9a086c4a6fb8e9da66143e88ab0de779
Yumina9 <saei5455@naver.com>
2021-03-12 17:45:47 +0900

    useContext in Action

---
+++ recoding/recoding_redoocks/src/context.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+
+export const UserContext = React.createContext();
+
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = useState({
+        name: "Mina",
+        loggedIn: false
+    });
+    const logUserIn = () => setUser({...user, loggedIn: true});
+    return (
+        <UserContext.Provider value={{user, logUserIn}}>{children}</UserContext.Provider>
+    )
+}
+
+export default UserContextProvider;
